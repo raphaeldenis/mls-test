600c2a8f7717f61df08d3031a329d286b7a7e00c
rdenis <rdenisfr@gmail.com>
2021-10-11 10:17:17 +0200

    feat(store): add state for comics with vuex

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,12 +1,21 @@
 import { createStore } from 'vuex';
+import { Comics } from '@/types/Comics';
+
+export type State = { comics: Comics };
+
+// const state: State = { comics: [] };
 
 export default createStore({
-  state: {
-  },
+  state: { comics: [] },
   mutations: {
+    setComics(state: State, payload: Comics) {
+      state.comics = payload;
+    },
   },
   actions: {
+    setComics({ commit }) {
+      commit('setComics');
+    },
   },
-  modules: {
-  },
+  modules: {},
 });
