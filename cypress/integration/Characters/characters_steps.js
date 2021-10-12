48322ca4f875d94d7564eac0919863b080c0ed9f
rdenis <rdenisfr@gmail.com>
2021-10-12 17:17:24 +0200

    test(e2e): fix linter issue

--- cypress/integration/Characters/characters_steps.js
+++ cypress/integration/Characters/characters_steps.js
@@ -13,14 +13,14 @@ const positionStringtoPositionNumber = {
   tenth: 9,
 };
 
-And('I can see a list of {string}', (text) => {
-  cy.get(`[data-test-id="${text}-card"]`).should(($element) => {
+And('I can see a list of {string}', text => {
+  cy.get(`[data-test-id="${text}-card"]`).should($element => {
     expect($element).to.have.length(5);
   });
 });
 
 And('the {string} element title should be {string}', (index, cardname, text) => {
-  cy.get(`[data-test-id=" ${cardname}-card"]`).should(($element) => {
+  cy.get(`[data-test-id=" ${cardname}-card"]`).should($element => {
     expect($element.eq(positionStringtoPositionNumber[index]), `${index} item`).to.contain(text);
   });
 });
